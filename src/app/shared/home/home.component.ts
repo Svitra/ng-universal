93eb49108603626059109e05d3c770bddcc95994
Alicja Kempa <alicja.kempa@ikea.com>
2018-11-30 11:52:19 +0100

    Finished to stage 4

--- src/app/shared/home/home.component.ts
+++ src/app/shared/home/home.component.ts
@@ -1,11 +1,17 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  constructor() {
+  constructor(private title: Title, private meta: Meta) {
+    this.title.setTitle('Welcome to the Book App');
+    this.meta.updateTag({
+      name: 'description',
+      content: 'Welcome to the library',
+    });
   }
 }
